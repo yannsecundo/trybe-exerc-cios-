9cd0de58bb1a55c605232580877389b969fa3b57
yannsecundo <yannsecundo34@gmail.com>
2022-04-11 14:44:05 -0400

    requisito 3 e 4 realizados

--- Fundamentos/JavaScript-Objeto-Funcoes/script.js
+++ Fundamentos/JavaScript-Objeto-Funcoes/script.js
@@ -4,6 +4,7 @@ let player = {
      lastName: 'Silva',
      age: 34,
      medals: { golden: 2, silver: 3},
+     bestInTheWorld: [2006 , 2007, 2008, 2009, 2010, 2018],
 };
 
-console.log("A jogadora " + player.name + player.lastName + " tem " + player.age);
+console.log("A jogadora " + player.name + player.lastName + " foi eleita a melhor do mundo 6 vezes " + player.bestInTheWorld);
